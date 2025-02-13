3dfe879c2f25d5fd652f9dedce76e13a0ad2e730
aidencachia <aiden.cachia.23@um.edu.mt>
2025-02-13 15:47:55 +0100

    Add game.js and sepeate potato spinning algorithm

--- scripts/game.js
+++ scripts/game.js
@@ -1,35 +1,13 @@
-let speed = 0; // Initial speed
-let angle = 0; // Current rotation angle
-let spinning = false;
+let revs = 0;
 
-const spinningImage = "img/potato_spinning_100x100.webp";
-const stoppedImage = "img/potato_ideal_100x100.webp";
-
-function spin() {
-    if (speed > 0.01) { // Keep spinning until nearly stopped
-        angle += speed; // Increase rotation
-        speed *= 0.98; // Slow down gradually
-
-        document.getElementById("potato").style.transform = `rotate(${angle}deg)`;
-        
-        requestAnimationFrame(spin);
-    } else {
-        spinning = false; // Stop animation loop when speed is very low
-    }
-
-    if(speed < 0.5){
-        document.getElementById("potato").src = stoppedImage;
-    } else {
-        document.getElementById("potato").src = spinningImage;
-    }
+function addRevs(revs) {
+    this.revs += revs;
 }
 
+function removeRevs(revs) {
+    this.revs -= revs;
+}
 
-function increaseSpin(){
-    speed += 5; // Increase speed on click
-    if (!spinning) {
-        spinning = true;
-        spin();
-    }
-    console.log("Spin increased");
+function getRevCount() {
+    return this.revs;
 }
