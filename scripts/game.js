e14046df2a6a4545cdfde424e91e3860a698ce84
aidencachia <147393490+aidencachia@users.noreply.github.com>
2025-02-16 12:46:31 +0100

    Remove getters and setters

--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,1 @@
-let revs = 0;
-
-function addRevs(revs) {
-    this.revs += revs;
-}
-
-function removeRevs(revs) {
-    this.revs -= revs;
-}
-
-function getRevCount() {
-    return this.revs;
-}
+let revs = 0.00;
