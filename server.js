1d7386e4e83b9063933f2c0b95fc4e3404735bb7
aidencachia <147393490+aidencachia@users.noreply.github.com>
2025-02-18 21:49:08 +0100

    Change port to 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');;
 
-const port = 8080;
+const port = 80;
 
 app.get('/scripts/potato.js', (req, res) => {
     res.type('application/javascript'); // sets the Content-Type header
