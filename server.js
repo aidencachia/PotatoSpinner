99cfb835d668d589cbac6f2040dbab4723de4b1c
aidencachia <147393490+aidencachia@users.noreply.github.com>
2025-02-19 23:37:00 +0100

    Start Implement save functionality with UUID for data storage in data.json

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 const express = require('express');
 const app = express();
-const path = require('path');;
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const port = 80;
 
-app.get('/scripts/potato.js', (req, res) => {
-    res.type('application/javascript'); // sets the Content-Type header
-    res.sendFile(path.join(__dirname, 'client', 'scripts', 'potato.js'));
+app.use(express.json())
+
+app.post('/save', (req, res) => {
+  fs.readFile('data.json', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    const obj = JSON.parse(data);
+    let key;
+    do key = uuidv4(); while (Object.keys(obj).includes(key));
+    
+    const entry = {}
+    entry[key] = req.body;
+
+    fs.appendFile('data.json', JSON.stringify(entry)+',',
+    err => {
+        // Checking for errors 
+        if (err) throw err;
+
+        // Success 
+        console.log("Done writing");
+    });
+
+    console.log(entry);
+    res.location = "https://127.0.0.1/save?"+key;
+    res.status(201);
+    res.send(key);
   });
+});
+
+app.patch('/save', (req, res) => {
+  res.send(data);
+});
+
+app.get('/save', (req, res) => {
+  res.send(data);
+});
   
-  // Fallback: serve other static files normally
 app.use(express.static(path.join(__dirname, 'client')));
 
 app.listen(port, () => {console.log("Server started on port "+port)});
