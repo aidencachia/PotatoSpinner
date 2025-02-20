84616210bfc9d630cb694a1a3983b2e1edc98ffb
aidencachia <aiden.cachia.23@um.edu.mt>
2025-02-20 09:19:18 +0100

    Implement save and load functionality with UUID for user data management

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -9,6 +9,8 @@ let spinning = false;
 const spinningImage = "img/potato_spinning_100x100.webp";
 const stoppedImage = "img/potato_ideal_100x100.webp";
 
+window.onload = load();
+setInterval(save, 10000);
 
 function upgradeAutoMove() {
     if (revs >= Math.pow(1.8,autoMoveLevel)) {
@@ -81,22 +83,64 @@ function increaseSpin(){
     }
 }
 
-function save(){
+async function save(){
     if(localStorage.getItem("userId") == null){
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            revs: revs,
-            upgrades: {
-                autoMove: autoMoveLevel,
-                decreaseDeceleration: decreaseDecelerationLevel,
-                clickStrength: clickStrengthLevel
-            }
-          }),
-      };
-      fetch("http://127.0.0.1:80/save", requestOptions);
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                revs: revs,
+                upgrades: {
+                    autoMove: autoMoveLevel,
+                    decreaseDeceleration: decreaseDecelerationLevel,
+                    clickStrength: clickStrengthLevel
+                }
+            }),
+        };
+        const res = await fetch("http://127.0.0.1:80/save", requestOptions);
+        const data = await res.json();
+
+        console.log(data["key"]);
+
+        localStorage.setItem("userId", data["key"]);
+    }
+    else{
+        const requestOptions = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                revs: revs,
+                upgrades: {
+                    autoMove: autoMoveLevel,
+                    decreaseDeceleration: decreaseDecelerationLevel,
+                    clickStrength: clickStrengthLevel
+                }
+            }),
+        };
+        const res = await fetch("http://127.0.0.1:80/save?key="+localStorage.getItem("userId"), requestOptions);
+    }
+}
+
+async function load(){
+    if(localStorage.getItem("userId") != null){
+        const res = await fetch("http://127.0.0.1:80/save?key="+localStorage.getItem("userId"));
+        const data = await res.json();
+
+        revs = data["revs"];
+        autoMoveLevel = data["upgrades"]["autoMove"];
+        decreaseDecelerationLevel = data["upgrades"]["decreaseDeceleration"];
+        clickStrengthLevel = data["upgrades"]["clickStrength"];
+
+        document.getElementById("Revs").innerHTML = (Math.round(revs*100)/100).toFixed(2);
+        document.getElementById("autoMoveLevel").innerHTML = autoMoveLevel;
+        document.getElementById("autoMoveCost").innerHTML = (Math.ceil(Math.pow(1.8,autoMoveLevel)*100)/100).toFixed(2);
+        document.getElementById("decreaseDecelerationLevel").innerHTML = decreaseDecelerationLevel;
+        document.getElementById("decreaseDecelerationCost").innerHTML = (Math.ceil(Math.pow(1.8,decreaseDecelerationLevel)*100)/100).toFixed(2);
+        document.getElementById("clickStrengthLevel").innerHTML = clickStrengthLevel;
+        document.getElementById("clickStrengthCost").innerHTML = (Math.ceil(Math.pow(1.8,clickStrengthLevel)*100)/100).toFixed(2);
     }
 }
--- server.js
+++ server.js
@@ -12,14 +12,45 @@ app.post('/save', (req, res) => {
   fs.readFile('data.json', 'utf8', function (err, data) {
     if (err) throw err;
 
-    const obj = JSON.parse(data);
     let key;
+
+    const obj = JSON.parse(data);
     do key = uuidv4(); while (Object.keys(obj).includes(key));
     
     const entry = {}
     entry[key] = req.body;
 
-    fs.appendFile('data.json', JSON.stringify(entry)+',',
+    obj["data"][key] = req.body;
+
+    fs.writeFile('data.json', JSON.stringify(obj, null, "\t"),
+    err => {
+        // Checking for errors 
+        if (err) throw err;
+
+        // Success 
+        console.log("Done writing");
+    });
+    responseBody = {
+      key: key
+    }
+    res.status(201).send(responseBody);
+  });
+});
+
+app.patch('/save', (req, res) => {
+
+  fs.readFile('data.json', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    const obj = JSON.parse(data);
+    let key = req.query.key;
+    
+    const entry = {}
+    entry[key] = req.body;
+
+    obj["data"][key] = req.body;
+
+    fs.writeFile('data.json', JSON.stringify(obj, null, "\t"),
     err => {
         // Checking for errors 
         if (err) throw err;
@@ -28,21 +59,21 @@ app.post('/save', (req, res) => {
         console.log("Done writing");
     });
 
-    console.log(entry);
-    res.location = "https://127.0.0.1/save?"+key;
-    res.status(201);
-    res.send(key);
+    res.sendStatus(201);
   });
 });
 
-app.patch('/save', (req, res) => {
-  res.send(data);
-});
-
 app.get('/save', (req, res) => {
-  res.send(data);
+  fs.readFile('data.json', 'utf8', function (err, data) {
+    if (err) throw err;
+
+    const obj = JSON.parse(data);
+    let key = req.query.key;
+
+    res.status(201).send(obj["data"][key]);
+  });
 });
   
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.listen(port, () => {console.log("Server started on port "+port)});
+app.listen(port, "0.0.0.0", () => {console.log("Server started on port "+port)});
